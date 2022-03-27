04c3637d257eba5524c0fd41ee62236527e6bddc
Julian Espitia <julicam156@gmail.com>
2022-03-27 13:06:36 -0500

    Termine la interfaz de homescreen

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -32,9 +32,10 @@ class HomePage extends StatelessWidget {
           padding: const EdgeInsets.symmetric(horizontal: 30),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               const Padding(
-                padding: EdgeInsets.only(top: 30, bottom: 10),
+                padding: EdgeInsets.only(top: 30, bottom: 20),
                 child: Text(
                   'Recent played',
                   style: TextStyle(
@@ -57,7 +58,7 @@ class HomePage extends StatelessWidget {
                     ),
                     SizedBox(width: 12),
                     Card(
-                      color: Colors.deepPurple,
+                      color: Colors.pinkAccent,
                       name: 'Deep purple',
                       artist: 'The deeps',
                       icon: Icons.accessibility_new_rounded,
@@ -74,7 +75,7 @@ class HomePage extends StatelessWidget {
                 ),
               ),
               const Padding(
-                padding: EdgeInsets.only(top: 30, left: 0),
+                padding: EdgeInsets.only(top: 30, bottom: 20),
                 child: Text(
                   'RECOMMENDED',
                   style: TextStyle(
@@ -84,10 +85,152 @@ class HomePage extends StatelessWidget {
                   textAlign: TextAlign.start,
                 ),
               ),
+              Container(
+                height: 320,
+                child: GridView.count(
+                  scrollDirection: Axis.horizontal,
+                  crossAxisCount: 3,
+                  mainAxisSpacing: 80,
+                  childAspectRatio: 0.6,
+                  children: const [
+                    ListTileHomeScreen(
+                      icon: Icons.abc_outlined,
+                      title: 'ABC',
+                      subtitle: 'First Album',
+                      color: Colors.red,
+                    ),
+                    ListTileHomeScreen(
+                      icon: Icons.ac_unit_outlined,
+                      title: 'Ice',
+                      subtitle: 'First Album',
+                      color: Colors.grey,
+                    ),
+                    ListTileHomeScreen(
+                      icon: Icons.access_alarms,
+                      title: 'Clock',
+                      subtitle: 'First Album',
+                      color: Colors.lime,
+                    ),
+                    ListTileHomeScreen(
+                      icon: Icons.accessibility_new,
+                      title: 'Deep purple',
+                      subtitle: 'First Album',
+                      color: Colors.pink,
+                    ),
+                    ListTileHomeScreen(
+                      icon: Icons.add_alert_rounded,
+                      title: 'Alert',
+                      subtitle: 'Second Album',
+                      color: Colors.green,
+                    ),
+                    ListTileHomeScreen(
+                      icon: Icons.add_ic_call,
+                      title: 'Call me',
+                      subtitle: 'Second Album',
+                      color: Colors.purple,
+                    ),
+                    ListTileHomeScreen(
+                      icon: Icons.mark_as_unread_outlined,
+                      title: 'Mail',
+                      subtitle: 'Second Album',
+                      color: Colors.deepOrange,
+                    ),
+                    ListTileHomeScreen(
+                      icon: Icons.sports_cricket,
+                      title: 'Sport',
+                      subtitle: 'Second Album',
+                      color: Colors.yellowAccent,
+                    ),
+                    ListTileHomeScreen(
+                      icon: Icons.restore_page,
+                      title: 'Change me',
+                      subtitle: 'Second Album',
+                      color: Colors.pink,
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                height: 80,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: const BorderRadius.only(
+                    topRight: Radius.circular(25),
+                    topLeft: Radius.circular(25),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.2),
+                      blurRadius: 2,
+                    )
+                  ],
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ListTile(
+                      leading: Container(
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple[300],
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: const Icon(
+                          Icons.pedal_bike_rounded,
+                          size: 20,
+                        ),
+                      ),
+                      trailing: Icon(
+                        Icons.play_arrow_rounded,
+                        size: 50,
+                      ),
+                      title: Text('My own bike'),
+                      subtitle: Text('Street in town'),
+                    )
+                  ],
+                ),
+              )
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class ListTileHomeScreen extends StatelessWidget {
+  const ListTileHomeScreen({
+    Key? key,
+    required this.icon,
+    required this.title,
+    required this.subtitle,
+    required this.color,
+  }) : super(key: key);
+
+  final IconData icon;
+  final String title;
+  final String subtitle;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Container(
+        height: 50,
+        width: 50,
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Icon(
+          icon,
+          size: 20,
+        ),
+      ),
+      title: Text(title),
+      subtitle: Text(subtitle),
     );
   }
 }
