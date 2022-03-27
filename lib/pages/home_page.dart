12e884ac94a5a5f23734c467b44c9929f8d625b4
Julian Espitia <julicam156@gmail.com>
2022-03-27 10:59:21 -0500

    Se creo el widget de las canciones recien sonadas, pero se debe modificar, si no puedo, volvere a este commite jeje

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:music_app_ui/widgets/recent_played.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -33,9 +32,9 @@ class HomePage extends StatelessWidget {
           padding: const EdgeInsets.symmetric(horizontal: 30),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: const [
-              Padding(
-                padding: EdgeInsets.only(top: 30, left: 0),
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(top: 30, bottom: 10),
                 child: Text(
                   'Recent played',
                   style: TextStyle(
@@ -45,8 +44,36 @@ class HomePage extends StatelessWidget {
                   textAlign: TextAlign.start,
                 ),
               ),
-              RecentPlayWidget(),
-              Padding(
+              Container(
+                height: 140,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: const [
+                    Card(
+                      color: Colors.lightBlue,
+                      name: 'Sky blue',
+                      artist: 'The rollers',
+                      icon: Icons.safety_divider,
+                    ),
+                    SizedBox(width: 12),
+                    Card(
+                      color: Colors.deepPurple,
+                      name: 'Deep purple',
+                      artist: 'The deeps',
+                      icon: Icons.accessibility_new_rounded,
+                    ),
+                    SizedBox(width: 12),
+                    Card(
+                      color: Colors.lightGreen,
+                      name: 'Greens',
+                      artist: 'The Greens',
+                      icon: Icons.adb,
+                    ),
+                    SizedBox(width: 12),
+                  ],
+                ),
+              ),
+              const Padding(
                 padding: EdgeInsets.only(top: 30, left: 0),
                 child: Text(
                   'RECOMMENDED',
@@ -60,6 +87,59 @@ class HomePage extends StatelessWidget {
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class Card extends StatelessWidget {
+  const Card({
+    Key? key,
+    required this.color,
+    required this.name,
+    required this.artist,
+    required this.icon,
+  }) : super(key: key);
+
+  final IconData icon;
+  final Color color;
+  final String name;
+  final String artist;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      width: 150,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Icon(
+            icon,
+            size: 90,
+          ),
+          Text(
+            name,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+              color: Colors.white,
+            ),
+          ),
+          Text(
+            artist,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 15,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 7),
+        ],
       ),
     );
   }
